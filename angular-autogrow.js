bc89f547eb86c0585a7361309200c4218b4302c2
lokimeyburg <loki@aristolabs.com>
2013-05-17 14:34:13 -0700

    Simpler but more robust mirror. Still need to update on resize

--- angular-autogrow.js
+++ angular-autogrow.js
@@ -7,24 +7,22 @@ app.directive('autogrow', function() {
       paddingLeft = element.css('padding-left'),
       paddingRight = element.css('padding-right');
 
-    var $shadow = angular.element('<div></div>').css({
+    // style the mirror
+    var $mirror = angular.element('<div></div>').css({
       'font-size'  : element.css('font-size'),
       'font-family': element.css('font-family'),
       'width'      : element.css('width'),
-      'padding-top'   : element.css('padding-top'),
-      'padding-right' : element.css('padding-right'),
-      'padding-bottom': element.css('padding-bottom'),
-      'padding-left'  : element.css('padding-left'),
-      'line-height': element.css('padding-left'),
-      'overflow-x' : 'hidden',
-      'position'   : 'absolute',
-      'top'        : 0,
-      'left'   : -9999,
+      'padding'    : element.css('padding'),
+      'line-height': element.css('line-height'),
+      'position'   : 'relative',
       'white-space': 'pre-wrap',
       'word-wrap': 'break-word'
     });
-    angular.element(document.body).append($shadow);
 
+    // create the mirror
+    angular.element(document.body).append($mirror);
+
+    // update the mirror
     var update = function() {
       var times = function(string, number) {
         for (var i = 0, r = ''; i < number; i++) {
@@ -33,16 +31,17 @@ app.directive('autogrow', function() {
         return r;
       }
 
+      // send content to the mirror
       var val = element.val().replace(/</g, '&lt;')
         .replace(/>/g, '&gt;')
         .replace(/&/g, '&amp;')
         .replace(/\n$/, '<br/>&nbsp;')
         .replace(/\n/g, '<br/>')
         .replace(/\s{2,}/g, function(space) { return times('&nbsp;', space.length - 1) + ' ' });
-      $shadow.html(val);
+      $mirror.html(val);
 
-      console.log(element.css('width'));
-      element.css('height', Math.max($shadow[0].offsetHeight + 10 /* the "threshold" */, minHeight) + 'px');
+      // style the textarea
+      element.css('height', Math.max($mirror[0].offsetHeight + 10 /* the "threshold" */, minHeight) + 'px');
       element.css('overflow', 'hidden');
       element.css('display', 'block');
     }
