7ae8ee230659f98177ce4cc08b80aeab78d84e3e
lokimeyburg <loki@aristolabs.com>
2013-05-24 09:17:27 -0700

    Binding to window resizing changes

--- angular-autogrow.js
+++ angular-autogrow.js
@@ -13,10 +13,16 @@ app.directive('autogrow', function() {
       'font-family': element.css('font-family'),
       'width'      : element.css('width'),
       'padding'    : element.css('padding'),
-      'line-height': element.css('line-height'),
       'position'   : 'relative',
       'white-space': 'pre-wrap',
-      'word-wrap': 'break-word'
+      'word-wrap': 'break-word',
+      'border': element.css('border'),
+      // CSS3 specific styles
+      'box-sizing'          : element.css('box-sizing'),
+      '-moz-box-sizing'     : element.css('-moz-box-sizing'),
+      '-webkit-box-sizing'  : element.css('-webkit-box-sizing'),
+      'line-height'         : element.css('line-height'),
+
     });
 
     // create the mirror
@@ -40,13 +46,20 @@ app.directive('autogrow', function() {
         .replace(/\s{2,}/g, function(space) { return times('&nbsp;', space.length - 1) + ' ' });
       $mirror.html(val);
 
+      // sync mirror width
+      $mirror.css('width', element.css('width'));
+
       // style the textarea
       element.css('height', Math.max($mirror[0].offsetHeight + 10 /* the "threshold" */, minHeight) + 'px');
       element.css('overflow', 'hidden');
       element.css('display', 'block');
     }
 
+
+    // bind to keypresses and window width changes
     element.bind('keyup keydown keypress change', update);
+    $(window).bind("resize", update); // TODO: remove jquery dependency.
+
     update();
   }
 });
